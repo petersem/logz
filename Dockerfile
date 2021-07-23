cf925b867228098e946aad7840eb98a12637250c
Matthew Petersen <vms@pobox.com>
2021-07-23 12:46:39 +1000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:14-alpine
+
+# tzdata for timzone and net-tools
+RUN apk update 
+RUN apk add tzdata
+RUN apk add net-tools
+
+ENV NODE_ENV=production
+WORKDIR /usr/src/app
+COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
+RUN npm install --production --silent && mv node_modules ../
+COPY . .
+EXPOSE 3001
+CMD ["npm", "start"]
---
+++ index.js
@@ -0,0 +1,52 @@
+// index.js
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const cors = require("cors");
+
+// Express settings
+app.use(express.json());
+app.set("trust proxy", true);
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(cors());
+
+app.post("/pstr", (req,res) => {
+    let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    if(ip.includes(":")) ip = "localhost";
+    let version = req.body.version;
+    let nsEnabled = req.body.nsEnabled;
+    let odEnabled = req.body.odEnabled;
+    let sEnabled = req.body.sEnabled;
+    let rEnabled = req.body.rEnabled;
+    let pEnabled = req.body.pEnabled;
+    res.sendStatus(200);
+    let d = new Date();
+    d.getDate();
+    let now = d.toISOString();
+    let payload = now + "," + ip + "," + version + "," + nsEnabled + "," + odEnabled + "," + sEnabled + "," + rEnabled + "," + pEnabled + "\n"
+    fs.appendFile('./logs/posterr.txt', payload , function (err) {
+        if (err) throw err;
+      });
+});
+
+app.post("/test", (req,res) => {
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    res.write("Your IP is:",ip);
+    res.write("Functioning ok");
+    res.sendStatus(200);
+
+    fs.appendFile('posterr.txt', payload , function (err) {
+        if (err) throw err;
+      });
+});
+
+
+// start listening on port 3000
+app.listen(3001, () => {
+    console.log(`✅ Web server started on internal port 3001 `);
+});
+
